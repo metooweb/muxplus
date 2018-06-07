3a1fa66f7204da5cbe8bd775d6f26dc728828679
oldtailor <1058776960@qq.com>
2018-06-07 15:07:19 +0800

    example

--- example/main.go
+++ example/main.go
@@ -5,37 +5,59 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"context"
+	"encoding/json"
 )
 
-func main() {
-
-	router := mux.NewRouter()
-	handler := muxplus.DefaultArgsParseHandler(
-		Test(
-			muxplus.DefaultServerHandler{},
-		),
-	)
-
-	muxplus.HandleFuncPlus(router, "/", func(ctx context.Context) (int, error) {
-
-		return 1, nil
-
-	}, handler)
-
-	http.ListenAndServe(":81", router)
-
+type OptionHello struct {
+	Name string `json:"name" form:"name"`
 }
 
-func Test(next muxplus.Handler) muxplus.Handler {
+type ResHello struct {
+	Msg string
+}
+
+func Hello(ctx context.Context, options *OptionHello) (res *ResHello, err error) {
+
+	res = &ResHello{}
+
+	res.Msg = "hello world " + options.Name
+
+	return
+}
+
+func ResponseHandler(next muxplus.Handler) muxplus.Handler {
 
 	return muxplus.HandlerFunc(func(w http.ResponseWriter, req *http.Request, fv *muxplus.FuncVal) {
 
 		next.Deal(w, req, fv)
 
-		if !fv.Outs[0].IsNil() {
+		if resErr := fv.Outs[ 1 ]; !resErr.IsNil() {
+
+			http.Error(w, resErr.String(), http.StatusInternalServerError)
+
+		} else {
+
+			if err := json.NewEncoder(w).Encode(fv.Outs[0].Interface()); err != nil {
+				panic(err)
+			}
 
 		}
 
 	})
 
+}
+
+func main() {
+
+	handler := muxplus.DefaultArgsParseHandler(
+		ResponseHandler(
+			muxplus.DefaultServerHandler{},
+		),
+	)
+
+	router := mux.NewRouter()
+
+	muxplus.HandleFuncPlus(router, "/", Hello, handler)
+
+	http.ListenAndServe(":8081", router)
 }
